f2ddc5209df158e41b50af6de7a32af310761609
selda kabir <seldaakabirnejat@gmail.com>
2024-07-02 19:29:14 +0300

    addState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new incomes 🤑",
+];
+
+export default function App() {
+  const [step, setStep] = useState(1)
+  function handlerNext() {
+    if(step<3)setStep(step+1)
+
+  }
+  function handlerPrevious() {
+    if(step>1) setStep(step-1)
+  }
+   return (
+     <div className="steps">
+       <div className="numbers">
+         <div >1</div>
+         <div >2</div>
+         <div >3</div>
+     </div>
+       <p className="message">Step{step} :{messages[step-1]}</p>
+       <div className="buttons">
+         <button style={{backgroundColor: '#7950f2',color:'#fff'}} onClick={handlerPrevious}>Previous</button>
+         <button style={{backgroundColor: '#7950f2',color:'#fff'}} onClick={handlerNext}>Next</button>
+     </div>
+     </div>
+  )
 }
-
-export default App;
