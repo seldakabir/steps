ace0921081499ab7c3579adeaece3f33a64cceb1
selda kabir <seldaakabirnejat@gmail.com>
2024-07-02 19:32:53 +0300

    addButtClass

--- src/App.js
+++ src/App.js
@@ -18,9 +18,9 @@ export default function App() {
    return (
      <div className="steps">
        <div className="numbers">
-         <div >1</div>
-         <div >2</div>
-         <div >3</div>
+         <div className={step>=1?'active':''}>1</div>
+         <div className={step>=2?'active':''}>2</div>
+         <div className={step>=3?'active':''}>3</div>
      </div>
        <p className="message">Step{step} :{messages[step-1]}</p>
        <div className="buttons">
