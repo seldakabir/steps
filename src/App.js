b51ba16baea1aa21a9e385536f6a3d6af63194f4
selda kabir <seldaakabirnejat@gmail.com>
2024-07-03 12:26:11 +0300

    addCloseOpen

--- src/App.js
+++ src/App.js
@@ -8,25 +8,33 @@ const messages = [
 
 export default function App() {
   const [step, setStep] = useState(1)
+  const [test, setTest] = useState({ name: 'Jonas' })
+  const [isOpen,setIsOpen]=useState(true)
   function handlerNext() {
-    if(step<3)setStep(step+1)
+    if (step < 3) setStep(step + 1)
+    setTest({name:'selda'})
 
   }
   function handlerPrevious() {
     if(step>1) setStep(step-1)
   }
-   return (
-     <div className="steps">
+  return (
+    <>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>&times;</button>
+    {isOpen &&(
+     <div className = "steps" >
        <div className="numbers">
          <div className={step>=1?'active':''}>1</div>
          <div className={step>=2?'active':''}>2</div>
          <div className={step>=3?'active':''}>3</div>
      </div>
-       <p className="message">Step{step} :{messages[step-1]}</p>
+       <p className="message">Step{step} :{messages[step - 1]}{ test.name}</p>
        <div className="buttons">
          <button style={{backgroundColor: '#7950f2',color:'#fff'}} onClick={handlerPrevious}>Previous</button>
          <button style={{backgroundColor: '#7950f2',color:'#fff'}} onClick={handlerNext}>Next</button>
      </div>
-     </div>
+        </div>
+     ) }
+      </>
   )
 }
